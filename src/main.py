fc48ced8e86897a42a0408a1d3d8d27263336278
Professor Xplorer <px@Professors-MacBook-Pro.local>
2024-06-20 08:27:35 +0530

    class cc update

--- src/main.py
+++ src/main.py
@@ -2,3 +2,12 @@
 print("Hello")
 
 print("New code")
+
+
+# git add <add Change>
+# git status <get status>
+# git commit -m "msg" <change msg>
+#  git push <push the chnage to remote repository >
+# git pull <pull the chnage from remote repo to local>
+# git checkout <change branch>
+# git checkout -b newbranchname <change branch with new branch from main > 
